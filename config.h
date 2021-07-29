a2736607d01ab330c7ca2a2ddad2a6229ff47bfe
okasurya <brilliantsurya@gmail.com>
2021-07-29 22:55:51 +0700

    add rofi

--- config.h
+++ config.h
@@ -21,7 +21,7 @@ static const unsigned int gappov    = 10;       /* vert outer gap between window
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Fira Mono for Powerline:size=11:antialias=true" };
+static const char *fonts[]          = { "Fira Mono for Powerline:size=11:antialias=true", "JoyPixels:pixelsize=10:antialias=true:autohint=true"};
 static const char dmenufont[]       = "Fira Mono for Powerline:size=11:antialias=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -92,11 +92,12 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", "--command", "tmux" };
+static const char *roficmd[]  = { "rofi", "-show", "combi" };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-        { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	/* { MODKEY,                       XK_p,      spawn,          SHCMD("rofi -combi-modi window,drun,ssh -font \"hack 10\" -show combi") }, */
+        /* { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } }, */
+	{ MODKEY,                       XK_p,      spawn,          {.v = roficmd} },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_s,      spawn,          SHCMD(SCREENSHOT) },
 	{ MODKEY,                       XK_w,      spawn,          SHCMD(BROWSER) },
